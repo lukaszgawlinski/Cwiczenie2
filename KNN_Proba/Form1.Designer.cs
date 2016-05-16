9845daaf4fda47dee65d887aa4e539ac9e209b31
lukaszgawlinski <lukasz.gawlinskii@gmail.com>
2016-05-16 16:09:41 +0200

    Wersja JESZCZE_ROBIE

---
+++ KNN_Proba/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace KNN_Proba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.cbMetryki = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "wczytaj TST";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(11, 47);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(140, 235);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(158, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 31);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "wczytaj TRN";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox2.Location = new System.Drawing.Point(158, 47);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(140, 235);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            this.openFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog2_FileOk);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(590, 259);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Zamknij";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(306, 107);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(185, 174);
+            this.richTextBox3.TabIndex = 5;
+            this.richTextBox3.Text = "";
+            this.richTextBox3.TextChanged += new System.EventHandler(this.richTextBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(389, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "wynik";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(590, 20);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Pomoc";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // cbMetryki
+            // 
+            this.cbMetryki.FormattingEnabled = true;
+            this.cbMetryki.Items.AddRange(new object[] {
+            "Metryka Euklidesowa",
+            "Metryka Canberra",
+            "Metryka Manhattan",
+            "Metryka Czebyszewa"});
+            this.cbMetryki.Location = new System.Drawing.Point(307, 22);
+            this.cbMetryki.Name = "cbMetryki";
+            this.cbMetryki.Size = new System.Drawing.Size(184, 21);
+            this.cbMetryki.TabIndex = 8;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(335, 47);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(67, 20);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(430, 51);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(60, 52);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Wykonaj";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(304, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "k = ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 290);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.cbMetryki);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "KNN";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox cbMetryki;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ KNN_Proba/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KNN_Proba
+{
+    public partial class Form1 : Form
+    {
+
+        //int x = 5;
+        public string wczytaj;
+        private int[][] systemTestowy;
+        private int[][] systemTreningowy;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var wynik = openFileDialog1.ShowDialog();
+            if (wynik != DialogResult.OK)
+                return;
+            if (wynik == DialogResult.OK)
+            {
+
+                systemTestowy = Wczytaj(openFileDialog1.FileName, richTextBox1);
+                
+            }
+        }
+
+
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var wynik = openFileDialog2.ShowDialog();
+            if (wynik != DialogResult.OK)
+                return;
+            if (wynik == DialogResult.OK)
+            {
+
+                systemTreningowy = Wczytaj(openFileDialog2.FileName, richTextBox2);
+
+            }
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        int [][] Wczytaj(string path, RichTextBox rtb = null)
+        {
+            string trescPliku = System.IO.File.ReadAllText(path);
+            if (rtb != null)
+                rtb.AppendText(trescPliku);
+
+            string[] poziomy = trescPliku.Split('\n');
+            int[][] daneZPliku = new int[poziomy.Length][];
+            for (int i = 0; i < poziomy.Length; i++)
+            {
+                string poziom = poziomy[i].Trim();
+                string[] miejscaParkingowe = poziom.Split(' ');
+                daneZPliku[i] = new int[miejscaParkingowe.Length];
+                for (int j = 0; j < miejscaParkingowe.Length; j++)
+                {
+                    daneZPliku[i][j] = int.Parse(miejscaParkingowe[j]);
+                }
+            }
+
+            return daneZPliku;
+        }
+
+        private void cbMetryki_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Pomoc");
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //richTextBox3.Clear();
+            foreach (var item in systemTestowy)
+            {
+                KlasyfikujObiekt(item, systemTreningowy, (int)numericUpDown1.Value); //wywolanie funkcji do testow
+                
+                //richTextBox3.Text += string.Join(" ", KlasyfikujObiekt(item, systemTreningowy, (int)numericUpDown1.Value));
+                //richTextBox3.Text += "\n";
+            }
+
+        }
+        
+        private double metrykaManhattan(int[] x, int[] y)
+        {
+            double wynik = 0;
+            for (int i = 0; i < x.Length - 1; i++)
+            {
+                wynik += Math.Abs(x[i] - y[i]);
+            }
+            return wynik;
+        }
+
+        private double metrykaEuklidesowa(int[] x, int [] y)
+        {
+            double wynik = 0;
+            for (int i = 0; i < x.Length - 1; i++)
+            {
+                wynik += Math.Pow(x[i] - y[i], 2);
+
+            }
+            return Math.Sqrt(wynik);
+
+        }
+        private double metrykaCanberra(int[] x, int [] y)
+        {
+            double wynik = 0;
+            for (int i = 0; i < x.Length - 1; i++)
+            {
+                wynik += Math.Abs((double)(x[i] - y[i]) / (double)(x[i] + y[i]));
+            }
+            return wynik;
+
+        }
+        private double metrykaCzebyszewa(int[] x, int[] y)
+        {
+            double[] wyniki = new double[x.Length - 1];
+            for (int i = 0; i < x.Length - 1; i++)
+            {
+                wyniki[i] = Math.Abs(x[i] - y[i]);
+            }
+            return wyniki.OrderByDescending(z => z).First(); // sortuje po wynikach i wybieram pierwszy
+        }
+
+
+
+
+        int? KlasyfikujObiekt(int[] obiekt, int[][] TRN, int k)
+        {
+
+            List<double> wyniki = new List<double>(); // tworzenie listy doubli 
+            foreach (var item in TRN)
+            {
+                switch (cbMetryki.SelectedIndex)
+                {
+                    case 0:
+                        {
+                            wyniki.Add(metrykaEuklidesowa(obiekt, item));
+                            break;
+                        }
+                    case 1:
+                        {
+                            wyniki.Add(metrykaCanberra(obiekt, item));
+                            break;
+                        }
+                    case 2:
+                        {
+                            wyniki.Add(metrykaManhattan(obiekt, item));
+                            break;
+                        }
+                    case 3:
+                        {
+                            wyniki.Add(metrykaCzebyszewa(obiekt, item));
+                            break;
+                        }            
+                    default:
+                        break;
+                }
+
+            }
+
+            return 1;
+        }
+    }
+}
